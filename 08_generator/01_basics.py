e9906277992bd61a26299edef8fdabdc3e7301e5
Harshraj35 <rajharsh3536@gmail.com>
2026-04-04 21:47:50 +0530

    python

---
+++ 08_generator/01_basics.py
@@ -0,0 +1,21 @@
+def serve_chai():
+    yield "Cup 1: Masala chai"
+    yield "Cup 2: Ginger chai"
+    yield "Cup 3: Cardamom chai"
+
+stall = serve_chai()
+
+for cup in stall:
+    print(cup)
+
+def get_chai_list():
+    return ["Cup 1", "Cup 2", "Cup 3"]
+
+
+def get_chai_get():
+    yield "Cup 1"
+    yield "Cup 2"
+    yield "Cup 3"
+
+chai = get_chai_list()
+print(chai)    
---
+++ 08_generator/03_send_gen.py
@@ -0,0 +1,12 @@
+def chai_customer():
+    print("Welcome ! What chai would you like ?")
+    order = yield
+    while True:
+        print(f"Preparing your {order} chai")
+        order = yield
+
+stall = chai_customer()
+next(stall)
+
+stall.send("Masala chai")
+stall.send("Ginger chai")
