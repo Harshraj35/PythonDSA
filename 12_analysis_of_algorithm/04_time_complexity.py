8988fe46695ee13efbf750dacbb72e91f6cd5b71
Harshraj35 <rajharsh3536@gmail.com>
2026-05-05 12:51:09 +0530

    Add initial implementations for time complexity examples

---
+++ 12_analysis_of_algorithm/04_time_complexity.py
@@ -0,0 +1,6 @@
+def main(n = 5):
+    
+    i = n
+    while (i > 1):
+        i = i - 1
+    print("Result:", i)
---
+++ 12_analysis_of_algorithm/05_time_complexity.py
@@ -0,0 +1,6 @@
+def main(n = 10):
+
+    i = n
+    while (i >= 1):
+        i = i - 2
+    print("Result:", i)
